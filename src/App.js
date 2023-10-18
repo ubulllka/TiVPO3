1fdadd518f23a53df7ea257b819d16353c95900d
ubulllka <ulyana.bulgakovaa@yandex.ru>
2023-10-18 15:32:52 +0300

    square

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+
+export function Square() {
+  return (
+    <button role="square" className="square">X</button>
+  );
+}
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import {Square } from './App';
 
-
+test('renders learn react link', () => {
+  render(<Square />);
+  const linkElement = screen.getByRole('square');
+  userEvent.click(linkElement)
+});
