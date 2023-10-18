ad16e55521b0f5c284ee76a35768b73d46fcce5c
ubulllka <ulyana.bulgakovaa@yandex.ru>
2023-10-18 17:30:51 +0300

    calclate winner

--- src/App.js
+++ src/App.js
@@ -10,10 +10,16 @@ export function Square({value, handleClick}) {
 
 export function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
     function handleClick(i) {
         const nextSquares = squares.slice();
-        nextSquares[i] = "X";
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
         setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
     return (
         <div className="board">
@@ -34,4 +40,24 @@ export function Board() {
             </div>
             </div>
     );
+}
+
+export function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import userEvent from "@testing-library/user-event";
 import ReactDOM from 'react-dom/client';
-import {Square, Board } from './App';
+import {Square, Board, calculateWinner } from './App';
 import { act } from 'react-dom/test-utils';
 
 let container;
@@ -15,20 +15,59 @@ afterEach(() => {
   container = null;
 });
 
-describe('Square', () => {
-  it('Board render: ', () => {
+describe('square', () => {
+  it('square click: ', () => {
     act(() => {
       ReactDOM.createRoot(container).render(<Board />);
     });
-
     const squares = container.querySelectorAll(".square");
     act(() => {
       squares[5].click();
     });
-
     expect(squares[5].innerHTML).toBe("X")
   });
+
+
+  it('square click (x,o,x,o): ', () => {
+    act(() => {
+      ReactDOM.createRoot(container).render(<Board />);
+    });
+    const squares = container.querySelectorAll(".square");
+    act(() => {
+      squares[5].click();
+    });
+    expect(squares[5].innerHTML).toBe("X");
+    act(() => {
+      squares[6].click();
+    });
+    expect(squares[6].innerHTML).toBe("O");
+  });
 });
+
+describe("Calculate winner", () => {
+  it("null", () => {
+    const array = ["X", "X", "O", "O", "O", "X", "X", "O", "X"];
+    // X X O
+    // O O X
+    // X O X
+    expect(calculateWinner(array)).toBe(null)
+  })
+  it("X", () => {
+    const array = ["X", "X", "X", "O", "O", "X", "X", "O", "O"];
+    // X X X
+    // O O X
+    // X O O
+    expect(calculateWinner(array)).toBe("X")
+  })
+  it("O", () => {
+    const array = ["X", "X", "O", "O", "O", "X", "O", "O", "O"];
+    // X X O
+    // O O X
+    // O O O
+    expect(calculateWinner(array)).toBe("O")
+  })
+});
+
 
 
 
